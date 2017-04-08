08b4a2cd112168ab8c9185c19017f4ea6733180e
Jake Bacher <jakebacher@yahoo.com>
2017-04-08 00:32:56 -0700

    server

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 // var exphbs = require('express-handlebars');
+
+
+
 var _ = require('underscore');
 
 var app = express();
@@ -13,11 +16,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // app.set('view engine', 'handlebars');
 app.use('/public', express.static('public'));
 
-/* Similar to the pokemon project, we are loading in the data to the
- * _DATA object. Luckily, we will never actually have to save new data
- * to a file. */
 
-app.get('/', function(req, res) {
+app.get('/api/generate', function(req, res) {
     /* For this endpoint, all you have to do is return the states, and
      * whatever information is necessary to link them to their own
      * /state/:statename endpoint. */
