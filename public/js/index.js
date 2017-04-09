8145ac47f12f2335b38ff84de85dcd3f18523b49
garbageman <dnagle@umd.edu>
2017-04-09 09:21:09 -0400

    Added information

--- public/js/index.js
+++ public/js/index.js
@@ -4,12 +4,8 @@ $('#activate').click(function() {
   $.ajax({
     url: '/api/generate',
     complete: function(data) {
-      // console.log(data.responseJSON.string);
       /* Stupid hack no one should use */
       $("body /deep/ .Tweet-text").text(data.responseJSON.string);
-      // $("body /deep/ .dt-updated").text(randomDate().dateString());
-      // $("body /deep/ .TweetAction--retweet .TweetAction-stat").text(random.integer(0, 50000));
-      // $("body /deep/ .TweetAction--heart .TweetAction-stat").text(random.integer(0, 250000));
       $(".inner-container").css("display","inline-block");
       $(".loading-container").css("display","none");
     },
