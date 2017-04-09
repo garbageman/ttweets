c15dc79a0fd69175de2b88d4a43031ff95d7b39d
garbageman <dnagle@umd.edu>
2017-04-08 23:13:41 -0400

    Added content for chain explanation and trump loading gif

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,21 @@
 $('#activate').click(function() {
+  $(".inner-container").css("display","none");
+  $(".loading-container").css("display","block");
   $.ajax({
     url: '/api/generate',
-    complete: function(data){
-      console.log(data.responseJSON.string);
-      $('#tweet').text(data.responseJSON.string);
+    complete: function(data) {
+      // console.log(data.responseJSON.string);
+      /* Stupid hack no one should use */
       $("body /deep/ .Tweet-text").text(data.responseJSON.string);
+      // $("body /deep/ .dt-updated").text(randomDate().dateString());
+      // $("body /deep/ .TweetAction--retweet .TweetAction-stat").text(random.integer(0, 50000));
+      // $("body /deep/ .TweetAction--heart .TweetAction-stat").text(random.integer(0, 250000));
+      $(".inner-container").css("display","inline-block");
+      $(".loading-container").css("display","none");
     },
     error: function(data) {
       console.log('ERROR');
       console.log(data);
     }
   })
-})
+});
